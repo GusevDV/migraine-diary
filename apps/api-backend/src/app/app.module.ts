65397b73fc574fae54691a6b9add9e8b42689131
Dmitry Gusev <dvgusev@hotmail.com>
2023-08-20 23:35:30 +0300

    Updated db config

--- apps/api-backend/src/app/app.module.ts
+++ apps/api-backend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import databaseConfig from '../config/database.config';
+import databaseConfig, { getMongoConfig } from '../config/database.config';
 import { ImportsModule } from '../import/imports.module';
 
 process.env.DATABASE_HOST;
@@ -14,19 +14,8 @@ process.env.DATABASE_HOST;
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        const username = configService.get<string>('database.username');
-        const password = configService.get<string>('database.password');
-        const host = configService.get<string>('database.host');
-        const port = configService.get<number>('database.port');
-        const dbName = configService.get<string>('database.dbName');
-
-        return {
-          uri: `mongodb://${username}:${password}@${host}:${port}`,
-          dbName,
-        };
-      },
       inject: [ConfigService],
+      useFactory: getMongoConfig,
     }),
     ImportsModule,
   ],
--- apps/api-backend/src/config/database.config.ts
+++ apps/api-backend/src/config/database.config.ts
@@ -1,4 +1,5 @@
-import { registerAs } from '@nestjs/config';
+import { ConfigService, registerAs } from '@nestjs/config';
+import { MongooseModuleOptions } from '@nestjs/mongoose';
 
 export default registerAs('database', () => ({
   host: process.env.DB_HOST,
@@ -7,3 +8,23 @@ export default registerAs('database', () => ({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
 }));
+
+const getMongoOptions = () => ({
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+export const getMongoConfig = async (
+  configService: ConfigService
+): Promise<MongooseModuleOptions> => {
+  const username = configService.get<string>('database.username');
+  const password = configService.get<string>('database.password');
+  const host = configService.get<string>('database.host');
+  const port = configService.get<number>('database.port');
+  const dbName = configService.get<string>('database.dbName');
+  return {
+    uri: `mongodb://${username}:${password}@${host}:${port}`,
+    dbName,
+    ...getMongoOptions(),
+  };
+};
